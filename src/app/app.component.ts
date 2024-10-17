2c00496cdd1794005b4d98217c14f99d936da6f0
Cristiano Barros <166430343+barrosCristiano@users.noreply.github.com>
2024-10-17 16:58:12 -0300

    Componente sobre adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    BannerComponent
+    BannerComponent,
+    AboutComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
